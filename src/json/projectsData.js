e95546f8d7c9a191d9f928b73f83c442e222d847
devSubham35 <devsubham35@gmail.com>
2025-05-27 12:39:37 +0530

    Modified structure

--- src/json/projectsData.js
+++ src/json/projectsData.js
@@ -8,48 +8,48 @@ export const projectsData = [
     previewImageUrl: "/images/Aconews.png",
     isDisabled: false,
   },
-  {
-    title: "CASA",
-    description:
-      "CASA makes ticketing simple for event organisers through its powerful and tailored platform. Whether it’s an intimate gig or a large-scale festival, CASA has everything you need to sell tickets with confidence. We’re free for event organisers, and customers know exactly what they’re paying for—no hidden fees, ever.",
-    technologies: [
-      "Next.js",
-      "TypeScript",
-      "GraphQL",
-      "MUI",
-      "Zustand",
-      "TanStack Query",
-    ],
-    liveUrl: "https://casa.fm/",
-    previewImageUrl: "/images/Casa.png",
-    isDisabled: false,
-  },
-  {
-    title: "Ganesh Enterprise",
-    description:
-      "Ganesh Grains is a leading Indian food brand established in 1936, known for producing high-quality flour, sattu, and spices. With state-of-the-art manufacturing facilities and a strong distribution network, the company ensures premium food products for consumers across India.",
-    technologies: [
-      "Next.js",
-      "TypeScript",
-      "TanStack Query",
-      "Redux",
-      "Material-UI",
-      "REST API",
-      "Framer Motion",
-    ],
-    liveUrl: "https://ganeshconsumer.com/",
-    previewImageUrl: "/images/Ganesh.png",
-    isDisabled: false,
-  },
-  {
-    title: "TUTR",
-    description:
-      "A responsive fully functional online teaching platform with its own currency coin for payments. Including Student portal, Teacher portal, Admin portal.",
-    technologies: ["React", "TypeScript", "Redux", "MUI", "REST API"],
-    liveUrl: "https://tutr.app/",
-    previewImageUrl: "/images/Tutr.png",
-    isDisabled: false,
-  },
+  // {
+  //   title: "CASA",
+  //   description:
+  //     "CASA makes ticketing simple for event organisers through its powerful and tailored platform. Whether it’s an intimate gig or a large-scale festival, CASA has everything you need to sell tickets with confidence. We’re free for event organisers, and customers know exactly what they’re paying for—no hidden fees, ever.",
+  //   technologies: [
+  //     "Next.js",
+  //     "TypeScript",
+  //     "GraphQL",
+  //     "MUI",
+  //     "Zustand",
+  //     "TanStack Query",
+  //   ],
+  //   liveUrl: "https://casa.fm/",
+  //   previewImageUrl: "/images/Casa.png",
+  //   isDisabled: false,
+  // },
+  // {
+  //   title: "Ganesh Enterprise",
+  //   description:
+  //     "Ganesh Grains is a leading Indian food brand established in 1936, known for producing high-quality flour, sattu, and spices. With state-of-the-art manufacturing facilities and a strong distribution network, the company ensures premium food products for consumers across India.",
+  //   technologies: [
+  //     "Next.js",
+  //     "TypeScript",
+  //     "TanStack Query",
+  //     "Redux",
+  //     "Material-UI",
+  //     "REST API",
+  //     "Framer Motion",
+  //   ],
+  //   liveUrl: "https://ganeshconsumer.com/",
+  //   previewImageUrl: "/images/Ganesh.png",
+  //   isDisabled: false,
+  // },
+  // {
+  //   title: "TUTR",
+  //   description:
+  //     "A responsive fully functional online teaching platform with its own currency coin for payments. Including Student portal, Teacher portal, Admin portal.",
+  //   technologies: ["React", "TypeScript", "Redux", "MUI", "REST API"],
+  //   liveUrl: "https://tutr.app/",
+  //   previewImageUrl: "/images/Tutr.png",
+  //   isDisabled: false,
+  // },
   {
     title: "Parkinlob",
     description:
@@ -59,33 +59,33 @@ export const projectsData = [
     previewImageUrl: "/images/Parkinlob.png",
     isDisabled: true,
   },
-  {
-    title: "Privy",
-    description:
-      "We are built for growing creators' audiences. Privy is the #1 influencer reviews platform that lets users review their favourite digital creators.",
-    technologies: ["Next.js", "TypeScript", "TanStack Query", "Redux", "MUI", "REST API"],
-    liveUrl: "https://privy.reviews/",
-    previewImageUrl: "/images/Privy.png",
-    isDisabled: false,
-  },
-  {
-    title: "Saturn",
-    description:
-      "Career Astrology helps you understand your strengths and what challenges you may face in your career. If you're feeling uncertain or are searching for answers, astrology can guide you and provide incredible insights into your career whether you're looking for a new role or starting a business.",
-    technologies: ["Next.js", "TypeScript", "TanStack Query", "Redux", "MUI", "REST API"],
-    liveUrl: "https://yoursaturn.com/",
-    previewImageUrl: "/images/Saturn.png",
-    isDisabled: false,
-  },
-  {
-    title: "VR-Bar",
-    description:
-      "VR-Bar is an interactive online dating game where users engage in fun, gamified matchmaking experiences. Through quizzes, challenges, and mini-games, players can discover compatible matches in an engaging and playful way.",
-    technologies: ["Next.js", "TypeScript", "Zustand", "Tailwind CSS", "Socket.IO"],
-    liveUrl: "#",
-    previewImageUrl: "/images/Vrbar.png",
-    isDisabled: true
-  },
+  // {
+  //   title: "Privy",
+  //   description:
+  //     "We are built for growing creators' audiences. Privy is the #1 influencer reviews platform that lets users review their favourite digital creators.",
+  //   technologies: ["Next.js", "TypeScript", "TanStack Query", "Redux", "MUI", "REST API"],
+  //   liveUrl: "https://privy.reviews/",
+  //   previewImageUrl: "/images/Privy.png",
+  //   isDisabled: false,
+  // },
+  // {
+  //   title: "Saturn",
+  //   description:
+  //     "Career Astrology helps you understand your strengths and what challenges you may face in your career. If you're feeling uncertain or are searching for answers, astrology can guide you and provide incredible insights into your career whether you're looking for a new role or starting a business.",
+  //   technologies: ["Next.js", "TypeScript", "TanStack Query", "Redux", "MUI", "REST API"],
+  //   liveUrl: "https://yoursaturn.com/",
+  //   previewImageUrl: "/images/Saturn.png",
+  //   isDisabled: false,
+  // },
+  // {
+  //   title: "VR-Bar",
+  //   description:
+  //     "VR-Bar is an interactive online dating game where users engage in fun, gamified matchmaking experiences. Through quizzes, challenges, and mini-games, players can discover compatible matches in an engaging and playful way.",
+  //   technologies: ["Next.js", "TypeScript", "Zustand", "Tailwind CSS", "Socket.IO"],
+  //   liveUrl: "#",
+  //   previewImageUrl: "/images/Vrbar.png",
+  //   isDisabled: true
+  // },
   {
     title: "ECME Template Integration",
     description: "Implemented and customized a robust admin dashboard using ECME a modern template, enabling seamless access to marketing, analytics, and AI tools. The integration ensures an efficient, scalable interface tailored for business operations.",
@@ -94,14 +94,14 @@ export const projectsData = [
     previewImageUrl: "/images/ECME.png",
     isDisabled: true
   },
-  {
-    title: "Jobreef",
-    description: "Developed a role-based job-finding platform that connects job seekers with relevant opportunities. Implemented a customizable admin dashboard for managing listings, users, and analytics, ensuring a streamlined experience for recruiters and applicants alike.",
-    technologies: ["React JS", "TypeScript", "Redux", "Tailwind CSS"],
-    liveUrl: "https://www.jobreef.net/",
-    previewImageUrl: "/images/Jobreef.png",
-    isDisabled: false
-  },
+  // {
+  //   title: "Jobreef",
+  //   description: "Developed a role-based job-finding platform that connects job seekers with relevant opportunities. Implemented a customizable admin dashboard for managing listings, users, and analytics, ensuring a streamlined experience for recruiters and applicants alike.",
+  //   technologies: ["React JS", "TypeScript", "Redux", "Tailwind CSS"],
+  //   liveUrl: "https://www.jobreef.net/",
+  //   previewImageUrl: "/images/Jobreef.png",
+  //   isDisabled: false
+  // },
   {
     title: "Dermavidya",
     description: "Built a dermatology-focused consulting platform enabling users to book consultations, access dermatology eBooks, join live conferences with top doctors, and collaborate with professionals. Designed a role-based interface for doctors, patients, and admins to ensure smooth navigation and functionality.",
